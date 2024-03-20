6f2535faa31968608cc3246b6e4c4f5c0fcb0b3f
Adam Pelsoczi <pelsoczad@gmail.com>
2024-03-20 22:36:42 +0100

    Redux RTKQuery api definition and store api reducer

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,10 +1,14 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import api from "./api";
 
-const rootReducer = combineReducers({})
+const rootReducer = combineReducers({
+    [api.reducerPath]: api.reducer
+})
 
 export const store = configureStore({
     reducer: rootReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+        .concat(api.middleware)
 })
 
 export type AppState = ReturnType<typeof store.getState>
