5aff139d9f88987f0ac02324f3892a67df18c3a2
Adam Pelsoczi <54379841+apelsoczi@users.noreply.github.com>
2024-03-22 12:11:14 +0100

    Server health checking for server availability with rtk query (#22)
    * Server availability health check rtk query endpoint and selectors

---
+++ src/lib/api/rebootedApi.ts
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const rebootedApi = createApi({
+    reducerPath: "api",
+    baseQuery: fetchBaseQuery({
+        baseUrl: process.env.REACT_APP_API_URL,
+        prepareHeaders: (headers) => {
+            headers.set('Content-Type', 'application/json');
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        getHealthCheck: builder.query<any, void>({
+            query: () => 'health'
+        })
+    })
+})
+
+export const {
+    useGetHealthCheckQuery
+} = rebootedApi
+
+export default rebootedApi
---
+++ src/lib/api/rebootedSlice.ts
@@ -0,0 +1,52 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { AppState } from '../redux/store';
+import rebootedApi from './rebootedApi';
+
+export interface RebootedState {
+    apiLoading: boolean,
+    apiAvailable: boolean
+}
+
+const initialState: RebootedState = {
+    apiLoading: true,
+    apiAvailable: false,
+}
+
+const rebootedSlice = createSlice({
+    name: 'rebooted',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addMatcher(
+            rebootedApi.endpoints.getHealthCheck.matchPending,
+            (state, { payload }) => {
+                state.apiLoading = true
+                state.apiAvailable = false
+            }
+        )
+        builder.addMatcher(
+            rebootedApi.endpoints.getHealthCheck.matchFulfilled,
+            (state, { payload }) => {
+                state.apiLoading = false
+                state.apiAvailable = true
+            }
+        )
+        builder.addMatcher(
+            rebootedApi.endpoints.getHealthCheck.matchRejected,
+            (state, { payload }) => {
+                state.apiLoading = false
+                state.apiAvailable = false
+            }
+        )
+    }
+})
+
+const selectServerLoading = (state: AppState) => state.rebooted.apiLoading
+const selectServerAvailable = (state: AppState) => state.rebooted.apiAvailable
+
+export {
+    selectServerLoading,
+    selectServerAvailable,
+}
+
+export default rebootedSlice
